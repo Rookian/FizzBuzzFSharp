c45e00b38e803c58e4c7ade190f6dc9829d46d61
Rookian <Rookian@gmx.de>
2016-10-31 21:44:40 +0100

    FlowDesignFSharp

---
+++ FlowDesignFSharp/Library1.fs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+
+namespace FlowDesignFSharp
+open System.IO  
+  module SpellChecker = 
+    
+    type Wörterbuch = {
+      Wörter : string list
+    }
+
+    let prüfe text = 
+      File.ReadLines("dictionary.txt")
+
+    let TextInWoerterZerlegen text = ""
+
+
+namespace FlowDesignFSharpTests
+open FlowDesignFSharp
+  module Tests = 
+
+  let ``Prüfen findet falsche Wörter`` () =
+    SpellChecker.prüfe "lorem ipsum dolor sit amet."
+    
